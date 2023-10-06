cdbd8b1a8575c14ecff8719690c6fffd24396c99
ZanCo21 <fauzan.alghifari21@gmail.com>
2023-10-06 13:56:42 +0700

    fix premission seeder (menambah group)

--- database/seeders/PermissionSeeder.php
+++ database/seeders/PermissionSeeder.php
@@ -18,84 +18,102 @@ class PermissionSeeder extends Seeder
 
     $permissions = [
 
-        // For Web
-        'can_access_web',
+        'web permissions' => [
+            'can_access_web',
+        ],
 
-        // For Divisions 
-        'view_divisions',
-        'add_divisions',
-        'edit_divisions',
-        'delete_divisions',
-        'import_divisions',
-        'export_divisions',
+        'divisions' => [
+            'view_divisions',
+            'add_divisions',
+            'edit_divisions',
+            'delete_divisions',
+            'import_divisions',
+            'export_divisions',
+        ],
 
-        // For Positions 
-        'view_positions',
-        'add_positions',
-        'edit_positions',
-        'delete_positions',
-        'import_positions',
-        'export_positions',
+        'positions' => [ 
+            'view_positions',
+            'add_positions',
+            'edit_positions',
+            'delete_positions',
+            'import_positions',
+            'export_positions',
+        ],
 
-        // For Employees
-        'view_employees',
-        'add_employees',
-        'edit_employees',
-        'delete_employees',
-        'import_employees',
-        'export_employees',
+        'employees' => [
+            'view_employees',
+            'add_employees',
+            'edit_employees',
+            'delete_employees',
+            'import_employees',
+            'export_employees',
+        ],
 
-        // For Presences
-        'view_presences',
-        'export_presences',
+        'presences' => [
+            'view_presences',
+            'export_presences',
+        ],
 
-        // For Partners
-        'view_partners',
-        'add_partners',
-        'edit_partners',
-        'delete_partners',
-        'import_partners',
-        'export_partners',
+        'partners' => [
+            'view_partners',
+            'add_partners',
+            'edit_partners',
+            'delete_partners',
+            'import_partners',
+            'export_partners',
+        ],
 
-        // For Projects
-        'view_projects',
-        'add_projects',
-        'edit_projects',
-        'delete_projects',
-        'import_projects',
-        'export_projects',
+        'projects' => [
+            'view_projects',
+            'add_projects',
+            'edit_projects',
+            'delete_projects',
+            'import_projects',
+            'export_projects',
+        ],
 
-        // For Standups
-        'view_standups',
-        'export_standups',
+        'standups' => [
+            'view_standups',
+            'export_standups',
+        ],
 
-        // For Roles
-        'view_roles',
-        'add_roles',
-        'edit_roles',
-        'delete_roles',
-        'import_roles',
-        'export_roles',
+        'roles' => [
+            'view_roles',
+            'add_roles',
+            'edit_roles',
+            'delete_roles',
+            'import_roles',
+            'export_roles',
+        ],
 
-        //Permission
-        'assign_permission',
+        'user permission' => [
+            'assign_permission',
+        ],
 
-        // APPROVE
-        'approve_preliminary', //untuk ht (status berubah dari pending ke preliminary)
-        'approve_allowed', //untuk hr (status berbentuk prelminary ke allowed)
-        'reject_presence',
+        'approve' => [
+            'approve_preliminary', //untuk ht (status berubah dari pending ke preliminary)
+            'approve_allowed', //untuk hr (status berbentuk prelminary ke allowed)
+            'reject_presence',
+        ],
 
-        //MOBILE||
-        'can_access_mobile',
+        'mobile access' => [
+            'can_access_mobile',
+        ],
 
-        //request 
-        'view_request_pending', // untuk status pending
-        'view_request_preliminary', // untuk status preliminary
+        'request presence' => [
+            'view_request_pending', // untuk status pending
+            'view_request_preliminary', // untuk status preliminary
+        ],
         
     ];
 
-    foreach ($permissions as $permission) {
-        Permission::create(['name' => $permission]);
+    foreach ($permissions as $group => $permissionList) {
+        foreach ($permissionList as $permission) {
+            Permission::create(['name' => $permission, 'group' => $group]);
+        }
     }
+    // foreach ($permissions as $permission) {
+    //     Permission::create(['name' => $permission]);
+    // }
 }
 }
