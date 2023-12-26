184e70396d738243f3e8107a5ef69a0795bfb8fd
arrapiii <mohamadarrafi1304@gmail.com>
2023-12-26 11:02:20 +0700

    fix: delete feature export pdf on table divisi, employee, partner, posisi, and project

--- resources/views/partner/index.blade.php
+++ resources/views/partner/index.blade.php
@@ -26,10 +26,6 @@
                                     <a href="" class="dropdown-item"> <i data-lucide="file-text"
                                             class="w-4 h-4 mr-2"></i> Export to Excel </a>
                                 </li>
-                                <li>
-                                    <a href="" class="dropdown-item"> <i data-lucide="file-text"
-                                            class="w-4 h-4 mr-2"></i> Export to PDF </a>
-                                </li>
                             @endcan
                             
                             @can('import_partners')
