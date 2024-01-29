c786dfccf534c848144d10e0d1e7410c9937ad8a
Mahesa Alfian Dhika <mahesaalfian03@gmail.com>
2024-01-29 10:37:49 +0700

    style : styling tampilan email sendOTP

--- resources/views/email/sendOtp.blade.php
+++ resources/views/email/sendOtp.blade.php
@@ -1,10 +1,196 @@
-<!-- resources/views/emails/namamailable.blade.php -->
-<!DOCTYPE html>
-<html>
+<!DOCTYPE HTML
+  PUBLIC "-//W3C//DTD XHTML 1.0 Transitional //EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xmlns:v="urn:schemas-microsoft-com:vml"
+  xmlns:o="urn:schemas-microsoft-com:office:office">
+
 <head>
-    <title>IMP APPROVAL OTP</title>
+  </xml>
+  <![endif]-->
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="x-apple-disable-message-reformatting">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title></title>
+
+  <style type="text/css">
+    @media only screen and (min-width: 620px) {
+      .u-row {
+        width: 600px !important;
+      }
+      .u-row .u-col {
+        vertical-align: top;
+      }
+      .u-row .u-col-100 {
+        width: 600px !important;
+      }
+    }
+    
+    @media (max-width: 620px) {
+      .u-row-container {
+        max-width: 100% !important;
+        padding-left: 0px !important;
+        padding-right: 0px !important;
+      }
+      .u-row .u-col {
+        min-width: 320px !important;
+        max-width: 100% !important;
+        display: block !important;
+      }
+      .u-row {
+        width: calc(100% - 40px) !important;
+      }
+      .u-col {
+        width: 100% !important;
+      }
+      .u-col>div {
+        margin: 0 auto;
+      }
+    }
+    
+    body {
+      margin: 0;
+      padding: 0;
+    }
+    
+    table,
+    tr,
+    td {
+      vertical-align: top;
+      border-collapse: collapse;
+    }
+    
+    p {
+      margin: 0;
+    }
+    
+    .ie-container table,
+    .mso-container table {
+      table-layout: fixed;
+    }
+    
+    * {
+      line-height: inherit;
+    }
+    
+    a[x-apple-data-detectors='true'] {
+      color: inherit !important;
+      text-decoration: none !important;
+    }
+    
+    table,
+    td {
+      color: #000000;
+    }
+    
+    a {
+      color: #04a768;
+      text-decoration: none;
+    }
+  </style>
 </head>
-<body>
-Your OTP code is: <b>{{ $otp }}</b>, You have 10 minutes to enter the OTP."
+
+<body class="clean-body u_body" style="margin: 0;padding: 0;-webkit-text-size-adjust: 100%;background-color: #f9f9f9;color: #000000">
+  <table style="border-collapse: collapse;table-layout: fixed;border-spacing: 0;mso-table-lspace: 0pt;mso-table-rspace: 0pt;vertical-align: top;min-width: 320px;Margin: 0 auto;background-color: #f9f9f9;width:100%" cellpadding="0" cellspacing="0">
+    <tbody>
+      <tr style="vertical-align: top">
+        <td style="word-break: break-word;border-collapse: collapse !important;vertical-align: top">
+
+          {{-- IMAGE --}}
+          <div class="u-row-container" style="padding: 0px;background-color: transparent">
+            <div class="u-row" style="Margin: 0 auto;min-width: 320px;max-width: 600px;overflow-wrap: break-word;word-wrap: break-word;word-break: break-word;background-color: #ffffff;">
+              <div style="border-collapse: collapse;display: table;width: 100%;background-color: transparent;">
+                <div class="u-col u-col-100" style="max-width: 320px;min-width: 600px;display: table-cell;vertical-align: top;">
+                  <div style="width: 100% !important;">
+                    <div style="padding: 0px;border-top: 0px solid transparent;border-left: 0px solid transparent;border-right: 0px solid transparent;border-bottom: 0px solid transparent;">
+                      <table style="font-family:'Cabin',sans-serif;" role="presentation" cellpadding="0" cellspacing="0" width="100%" border="0">
+                        <tbody>
+                          <tr>
+                            <td style="overflow-wrap:break-word;word-break:break-word;padding:20px;font-family:'Cabin',sans-serif;" align="left">
+                              <table width="100%" cellpadding="0" cellspacing="0" border="0">
+                                <tr>
+                                  <td style="padding-right: 0px;padding-left: 0px;" align="center">
+                                    <img align="center" border="0" src="https://admin.approval.impstudio.id/images/IMP-full.jpg" alt="Image" title="Image" style="outline: none;text-decoration: none;-ms-interpolation-mode: bicubic;clear: both;display: inline-block !important;border: none;height: auto;float: none;width: 50%;max-width: 1000px;"
+                                      width="179.2" />
+                                  </td>
+                                </tr>
+                              </table>
+                            </td>
+                          </tr>
+                        </tbody>
+                      </table>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          {{-- CCONTENT EMAIL --}}
+          <div class="u-row-container" style="padding: 0px; background-color: transparent;">
+            <div class="u-row" style="Margin: 0 auto; min-width: 320px; max-width: 600px; overflow-wrap: break-word; word-wrap: break-word; word-break: break-word; background-color: #ffffff;">
+                <div style="border-collapse: collapse; display: table; width: 100%; background-color: transparent;">
+                    <div class="u-col u-col-100" style="max-width: 320px; min-width: 600px; display: table-cell; vertical-align: top;">
+                        <div style="width: 100% !important;">
+                            <div style="padding: 0px; border-top: 0px solid transparent; border-left: 0px solid transparent; border-right: 0px solid transparent; border-bottom: 0px solid transparent;">
+                                <div style="padding: 10px; text-align: start; background-color: #1B253B;">
+                                  <p><br></p>
+                                  <p style="color: #CBD5E1; font-family: Verdana; font-size: 12px;margin-bottom: 3px">
+                                    <span style="font-weight: 600; font-size: 15px; color: #CBD5E1; text-align: start;text-transform:capitalize;">Hi, {{$user->name}}.</span> 
+                                  </p>
+                                  <p style="font-family: Verdana;">
+                                    <span style="font-weight: 400; color: #CBD5E1; font-size: 13px;">Kami mendapat permintaan reset password untuk email </span>
+                                  </p>
+                                  <p style="font-family: Verdana;">
+                                    <span style="font-weight: 400; color: #CBD5E1; font-size: 13px;"><span style="font-weight: 400; color: #CBD5E1; font-size: 13px;font-weight: bold">{{$user->email}} </span>Silakan gunakan kode OTP berikut</span>
+                                  </p>
+                                  <p style="font-family: Verdana;">
+                                    <span style="font-weight: 400; color: #CBD5E1; font-size: 13px;">untuk melanjutkan proses reset password:</span>
+                                  </p>
+                                  <p><br></p>
+                                  <p style="font-family: Verdana;">
+                                    <span style="font-weight: 400; color: #CBD5E1; font-size: 13px;">Kode OTP : <span style="font-weight: 400; color: #CBD5E1; font-size: 13px;font-weight: bold">{{ $otp }}</span></span>
+                                  </p>
+                                <p><br></p>
+                                <p style="font-family: Verdana;">
+                                  <span style="font-weight: 400; color: #CBD5E1; font-size: 13px;">Note : Kode OTP hanya berlaku dalam 10 menit </span>
+                                </p>
+                                  <p><br></p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+          </div>
+
+        {{-- FOOTER EMAIL --}}
+        <div class="u-row-container" style="padding: 0px;background-color: transparent">
+            <div class="u-row" style="Margin: 0 auto;min-width: 320px;max-width: 600px;overflow-wrap: break-word;word-wrap: break-word;word-break: break-word;background-color: #28334E;">
+                <div style="border-collapse: collapse;display: table;width: 100%;background-color: transparent;">
+                    <div class="u-col u-col-100" style="max-width: 320px;min-width: 600px;display: table-cell;vertical-align: top;">
+                        <div style="width: 100% !important;">
+                            <div style="padding: 0px;border-top: 0px solid transparent;border-left: 0px solid transparent;border-right: 0px solid transparent;border-bottom: 0px solid transparent;">
+                                <table style="font-family:'Cabin',sans-serif;" role="presentation" cellpadding="0" cellspacing="0" width="100%" border="0">
+                                    <tbody>
+                                        <tr>
+                                            <td style="overflow-wrap:break-word;word-break:break-word;padding:10px;font-family: Verdana;;" align="left">
+                                                <div style="color: #CBD5E1; line-height: 180%; text-align: center; word-wrap: break-word;">
+                                                    <p style="font-size: 14px; line-height: 180%;"><span style="font-size: 16px; line-height: 28.8px;">E-Approval • {{ \Carbon\Carbon::today()->format('d F Y') }}</span></p>
+                                                </div>
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </td>
+      </tr>
+    </tbody>
+  </table>
 </body>
+
 </html>
