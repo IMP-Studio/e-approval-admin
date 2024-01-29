dd14e1333b5d92949c4f6a41ad797d863c2f038f
Mahesa Alfian Dhika <mahesaalfian03@gmail.com>
2024-01-29 09:55:54 +0700

    feat : Job queue send email untuk request presensi

--- app/Mail/RequestPresenceEmail.php
+++ app/Mail/RequestPresenceEmail.php
@@ -12,7 +12,7 @@ use Illuminate\Queue\SerializesModels;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Contracts\Queue\ShouldQueue;
 
-class RequestPresenceEmail extends Mailable
+class RequestPresenceEmail extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
 
